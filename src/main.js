e7690db94308b25be08691172e9a2bd7d46ef1bf
carlosviniciusananias <carlos.oliveira69@fatec.sp.gov.br>
2019-12-14 18:57:51 -0300

    trabalhando com rotas

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+//importando rotas
+import VueRouter from 'vue-router'
+//para importar biblioteca para dentro do vue
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  //criando objeto vazio
+  routes: [
+    {
+      path: '/cadastro',
+      component: HelloWorld
+    }
+  ]
+})
 
 Vue.config.productionTip = false
 
 new Vue({
+  //passsando rota
+  router,
+
   render: h => h(App),
 }).$mount('#app')
