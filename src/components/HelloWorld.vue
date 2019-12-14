aece60363f76fd00e7e350864e0f148602ea6a02
carlosviniciusananias <carlos.oliveira69@fatec.sp.gov.br>
2019-12-14 18:51:45 -0300

    permitindo adicionar funcionário

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,6 +7,47 @@
         role="alert"
         v-show="funcionarios.length === 0"
       >Não há funcionários :(</div>
+
+      <div class="form-group">
+        <p>
+          <input
+            type="text"
+            placeholder="Digite o id do funcionário"
+            name="id"
+            class="form-control"
+            v-model="id"
+          />
+        </p>
+        <p>
+          <input
+            type="text"
+            placeholder="Digite o nome do funcionário"
+            name="nome"
+            class="form-control"
+            v-model="name"
+          />
+        </p>
+        <p>
+          <input
+            type="text"
+            placeholder="Digite o Sobrenome do funcionário"
+            name="sobrenome"
+            class="form-control"
+            v-model="lastname"
+          />
+        </p>
+        <p>
+          <input
+            type="text"
+            placeholder="Digite a idade do funcionário"
+            name="idade"
+            class="form-control"
+            v-model="age"
+          />
+        </p>
+        <button class="btn btn-success" v-on:click="add()">Inserir</button>
+      </div>
+
       <table class="table table-striped" v-show="funcionarios.length > 0">
         <thead>
           <tr>
@@ -23,9 +64,9 @@
             <td>{{ funcionario.id }}</td>
             <td>{{ funcionario.name }}</td>
             <td>{{ funcionario.lastname }}</td>
-            <td>{{ funcionario.idade }}</td>
+            <td>{{ funcionario.age }}</td>
             <td>
-              <button class="btn btn-danger" v-on:click="remove(index)">Remove</button>
+              <button class="btn btn-danger" v-on:click="remove(index)">Remover</button>
             </td>
           </tr>
         </tbody>
@@ -45,12 +86,16 @@ export default {
     return {
       titulo: "Funcionários",
       funcionarios: [
-        { id: 0, name: "Carlos", lastname: "Vinicius", idade: "22" },
-        { id: 1, name: "Felipe", lastname: "Vinicius", idade: "25" },
-        { id: 2, name: "Alan", lastname: "Vinicius", idade: "30" },
-        { id: 3, name: "João", lastname: "Vinicius", idade: "11" },
-        { id: 4, name: "Daniel", lastname: "Vinicius", idade: "59" }
-      ]
+        { id: 0, name: "Carlos", lastname: "Vinicius", age: "22" },
+        { id: 1, name: "Felipe", lastname: "Vinicius", age: "25" },
+        { id: 2, name: "Alan", lastname: "Vinicius", age: "30" },
+        { id: 3, name: "João", lastname: "Vinicius", age: "11" },
+        { id: 4, name: "Daniel", lastname: "Vinicius", age: "59" }
+      ],
+      id: "",
+      name: "",
+      lastname: "",
+      age: ""
     };
   },
   //aqui ficam todas as funções que seram definidas na página do vuejs
@@ -58,6 +103,21 @@ export default {
     remove(id) {
       //deletando posição quando encontra o valor
       this.funcionarios.splice(id, 1);
+    },
+
+    add() {
+      //adicionando novo elemento para o array
+      this.funcionarios.push({
+        id: this.id,
+        name: this.name,
+        lastname: this.lastname,
+        age: this.age
+      });
+
+      this.id = "";
+      this.name = "";
+      this.lastname = "";
+      this.age = "";
     }
   }
 };
